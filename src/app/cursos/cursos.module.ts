a58fd2203be40740af36f03a9abc8b410303453e
SergioAlarcon09 <sergio.alarcon01@uceva.edu.co>
2024-11-06 22:59:11 -0500

    Se añadio el modulo y servicios, junto con el componente de listar curso

---
+++ src/app/cursos/cursos.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CrearCursoComponent } from './crear-curso/crear-curso.component';
+import { DetalleCursoComponent } from './detalle-curso/detalle-curso.component';
+import { ListarCursoComponent } from './listar-curso/listar-curso.component';
+import { EditarCursoComponent } from './editar-curso/editar-curso.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {CursoRoutingModule} from './curso-routing.module';
+
+
+
+@NgModule({
+  declarations: [
+    CrearCursoComponent,
+    DetalleCursoComponent,
+    ListarCursoComponent,
+    EditarCursoComponent
+  ],
+  exports: [
+    ListarCursoComponent
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    CursoRoutingModule
+  ]
+})
+export class CursoModule { }
---
+++ src/app/cursos/detalle-curso/detalle-curso.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle-curso',
+  templateUrl: './detalle-curso.component.html',
+  styleUrl: './detalle-curso.component.css'
+})
+export class DetalleCursoComponent {
+
+}
